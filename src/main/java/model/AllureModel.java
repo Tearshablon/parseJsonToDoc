51745b80f4c4dce4eb0f040d3c1cd56b95c5d225
dshablov <shablov.d@gmail.com>
2018-08-13 19:56:51 +0300

    сделал модель

--- src/main/java/model/AllureModel.java
+++ src/main/java/model/AllureModel.java
@@ -19,5 +19,164 @@ public class AllureModel {
     private Boolean retry;
     private Extra extra;
     private String source;
-    private ParameterValues parameterValues;
+    private List<ParameterValues> parameterValues;
+
+    public AllureModel() {
+    }
+
+    public AllureModel(String uid, String name, String historyId, Time time, String status, Boolean flaky, List<BeforeStages> beforeStages, TestStage testStage, List<AfterStages> afterStages, List<Labels> labels, List<Parameters> parameters, List<Links> links, Boolean hidden, Boolean retry, Extra extra, String source, List<ParameterValues> parameterValues) {
+        this.uid = uid;
+        this.name = name;
+        this.historyId = historyId;
+        this.time = time;
+        this.status = status;
+        this.flaky = flaky;
+        this.beforeStages = beforeStages;
+        this.testStage = testStage;
+        this.afterStages = afterStages;
+        this.labels = labels;
+        this.parameters = parameters;
+        this.links = links;
+        this.hidden = hidden;
+        this.retry = retry;
+        this.extra = extra;
+        this.source = source;
+        this.parameterValues = parameterValues;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHistoryId() {
+        return historyId;
+    }
+
+    public void setHistoryId(String historyId) {
+        this.historyId = historyId;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Boolean getFlaky() {
+        return flaky;
+    }
+
+    public void setFlaky(Boolean flaky) {
+        this.flaky = flaky;
+    }
+
+    public List<BeforeStages> getBeforeStages() {
+        return beforeStages;
+    }
+
+    public void setBeforeStages(List<BeforeStages> beforeStages) {
+        this.beforeStages = beforeStages;
+    }
+
+    public TestStage getTestStage() {
+        return testStage;
+    }
+
+    public void setTestStage(TestStage testStage) {
+        this.testStage = testStage;
+    }
+
+    public List<AfterStages> getAfterStages() {
+        return afterStages;
+    }
+
+    public void setAfterStages(List<AfterStages> afterStages) {
+        this.afterStages = afterStages;
+    }
+
+    public List<Labels> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<Labels> labels) {
+        this.labels = labels;
+    }
+
+    public List<Parameters> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Parameters> parameters) {
+        this.parameters = parameters;
+    }
+
+    public List<Links> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<Links> links) {
+        this.links = links;
+    }
+
+    public Boolean getHidden() {
+        return hidden;
+    }
+
+    public void setHidden(Boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public Boolean getRetry() {
+        return retry;
+    }
+
+    public void setRetry(Boolean retry) {
+        this.retry = retry;
+    }
+
+    public Extra getExtra() {
+        return extra;
+    }
+
+    public void setExtra(Extra extra) {
+        this.extra = extra;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public List<ParameterValues> getParameterValues() {
+        return parameterValues;
+    }
+
+    public void setParameterValues(List<ParameterValues> parameterValues) {
+        this.parameterValues = parameterValues;
+    }
 }
--- src/main/java/model/History.java
+++ src/main/java/model/History.java
@@ -5,4 +5,28 @@ import java.util.List;
 public class History {
     private Statistic statistic;
     private List<Items> items;
+
+    public History() {
+    }
+
+    public History(Statistic statistic, List<Items> items) {
+        this.statistic = statistic;
+        this.items = items;
+    }
+
+    public Statistic getStatistic() {
+        return statistic;
+    }
+
+    public void setStatistic(Statistic statistic) {
+        this.statistic = statistic;
+    }
+
+    public List<Items> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Items> items) {
+        this.items = items;
+    }
 }
