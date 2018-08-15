f28e6e6544e1a8b9eb48329d8e651e792b940c42
dshablov <shablov.d@gmail.com>
2018-08-15 12:01:12 +0300

    фильтрация шагов без $

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import allureDTO.AllureModelDTO;
+import allureDTO.StepsDTO;
 import docgenerator.DocGenerator;
 import operationwithjson.JsonReader;
 
@@ -9,8 +10,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         List<AllureModelDTO> allureModel = JsonReader.getModelFromJson();
+        StepsDTO stepsDTO = getStepsDTOWithoutDollorSign(allureModel.get(0).getTestStage().getSteps().get(0));
+
 
         DocGenerator docGenerator = new DocGenerator();
         docGenerator.generateDocFiles(allureModel);
+    }
+
+    public static StepsDTO getStepsDTOWithoutDollorSign(StepsDTO steps) {
+        StepsDTO stepsDTO = steps;
+        for (int i = 0; i < stepsDTO.getSteps().size(); i++) {
+            if (stepsDTO.getSteps().get(i).getSteps().size() != 0) {
+                getStepsDTOWithoutDollorSign(stepsDTO.getSteps().get(i));
+            } else {
+                if (stepsDTO.getSteps().get(i).getName().contains("$")) {
+                    stepsDTO.getSteps().remove(i);
+                }
+            }
+        }
+        return stepsDTO;
     }
 }
--- src/main/java/docgenerator/DocGenerator.java
+++ src/main/java/docgenerator/DocGenerator.java
@@ -7,12 +7,17 @@ import org.apache.poi.xwpf.usermodel.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.List;
 
 public class DocGenerator {
 
     private XWPFDocument document;
     private final int PARAGRAPH_FONT_SIZE = 15;
+    private final String EPIC_NAME_FOR_DESCRIPTION_TABLE = "epic";
+    private final String STORY_NAME_FOR_DESCRIPTION_TABLE = "story";
+    private final String SEVERITY_NAME_FOR_DESCRIPTION_TABLE = "severity";
+    private final String URL_TO_KB_NAME_FOR_DESCRIPTION_TABLE = "tms";
 
     public DocGenerator() {
         this.document = new XWPFDocument();
@@ -28,6 +33,7 @@ public class DocGenerator {
 
     private void generateTestScenarioName(String testScenarioName) {
         XWPFParagraph paragraph = document.createParagraph();
+        paragraph.getCTP().getPPr().getNumPr().addNewIlvl().setVal(BigInteger.valueOf(1));
         XWPFRun xwpfRun = paragraph.createRun();
         xwpfRun.setText(testScenarioName);
         xwpfRun.setFontSize(PARAGRAPH_FONT_SIZE);
@@ -37,14 +43,27 @@ public class DocGenerator {
         XWPFTable table = document.createTable();
 
         XWPFTableRow tableRowOne = table.getRow(0);
-        String epic = JsonFilter.getEpicFromLabelByName(allureModel.getLabels());
-        String story = JsonFilter.getStoryFromLabelName(allureModel.getLabels());
-        String severity = JsonFilter.getSeverityFromLabelName(allureModel.getLabels());
-        String urlToKb = JsonFilter.getUrlToKbFromLinksByType(allureModel.getLinks());
+        tableRowOne.getCell(0).setText(EPIC_NAME_FOR_DESCRIPTION_TABLE.toUpperCase());
+        tableRowOne.addNewTableCell().setText(JsonFilter.getValueFromLabelByName(allureModel.getLabels(), EPIC_NAME_FOR_DESCRIPTION_TABLE));
+        tableRowOne.getCell(0).setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
 
-//        tableRowOne.getCell(0).setText();
-//        tableRowOne.addNewTableCell().setText();
-//        tableRowOne.addNewTableCell().setText();
+        XWPFTableRow tableRowTwo = table.createRow();
+        tableRowTwo.getCell(0).setText(STORY_NAME_FOR_DESCRIPTION_TABLE.toUpperCase());
+        tableRowTwo.getCell(1).setText(JsonFilter.getValueFromLabelByName(allureModel.getLabels(), STORY_NAME_FOR_DESCRIPTION_TABLE));
+
+        XWPFTableRow tableRowThree = table.createRow();
+        tableRowThree.getCell(0).setText(SEVERITY_NAME_FOR_DESCRIPTION_TABLE.toUpperCase());
+        tableRowThree.getCell(1).setText(JsonFilter.getValueFromLabelByName(allureModel.getLabels(), SEVERITY_NAME_FOR_DESCRIPTION_TABLE));
+
+
+        XWPFTableRow tableRowFour = table.createRow();
+        tableRowFour.getCell(0).setText(URL_TO_KB_NAME_FOR_DESCRIPTION_TABLE.toUpperCase());
+        tableRowFour.getCell(1).setText(JsonFilter.getUrlToKbFromLinksByType(allureModel.getLinks(), URL_TO_KB_NAME_FOR_DESCRIPTION_TABLE));
+
+//        String epic = JsonFilter.getValueFromLabelByName(allureModel.getLabels(), EPIC_NAME_FOR_DESCRIPTION_TABLE);
+//        String story = JsonFilter.getValueFromLabelByName(allureModel.getLabels(), STORY_NAME_FOR_DESCRIPTION_TABLE);
+//        String severity = JsonFilter.getValueFromLabelByName(allureModel.getLabels(), SEVERITY_NAME_FOR_DESCRIPTION_TABLE);
+//        String urlToKb = JsonFilter.getUrlToKbFromLinksByType(allureModel.getLinks());
     }
 
     public void createDocFile(int i) throws IOException {
